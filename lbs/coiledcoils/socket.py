9b0e852176788db961438a90ef8b8b6befa07477
Jan Ludwiczak <j.ludwiczak@cent.uw.edu.pl>
2018-08-20 23:01:29 +0200

    new socket parser

--- lbs/coiledcoils/socket.py
+++ lbs/coiledcoils/socket.py
@@ -1,118 +1,95 @@
 import re
-import pickle
-def parse_socket_output(filename, entry, method = 'cc'):
-    
+
+
+def parse_socket_output(filename, method='overlap'):
     """
-    filename: Socket output filename
-    entry: PDB entry name
-    method: Detection method
-        cc - take only the coiled coil assignment
-        kih - take the knobs into holes assignment
+    Parser of the Socket (J. Mol. Biol., 307 (5), 1427-1450) coiled coil assignment program
+    :param filename: Socket output (short version) filename
+    :param method: Assignment method
+                    'heptads' - based on the heptad assignment
+                    'knobs' - based on the knobs assignment
+                    'overlap' - sum of the ranges of 'heptads' and 'knobs'
+    :return: dict with the parsed coiled coils informations (if present) or 0 (if coiled coils absent)
     """
-    ### Read all lines from file
+    # Read all lines from file
     f = open(filename, 'r')
     lines = f.readlines()
     f.close()
-    
-    ### Get indices (first line numbers) for each CC assignment
-    start_indices = [] 
+
+    # Get indices (numbers of first line in file) for each CC assignment
+    start_indices = []
     for i in range(0, len(lines)):
         line = lines[i].rstrip()
         if line.startswith('coiled coil') and line.endswith(':'):
             start_indices.append(i)
-            #print(line)
         if 'Finished' in line:
             start_indices.append(i)
-    
+
     # Iterate over all CC assignments
     all_coils = []
     relations = []
-    for i in range(0, len(start_indices)-1): ### Last one is 'Finished' line
-        start = start_indices[i] # Starting line for CC assignment
-        stop = start_indices[i+1] # End line for CC assignment (next assignment starts or 'Finished')
-        
-        ### Data
-        coil_info = {}
-        coil_info['indices'] = []
-        coil_info['sequences'] = []
-        coil_info['heptads'] = []
-        coil_info['orientation'] = []
+    for i in range(0, len(start_indices) - 1):  # Last one is 'Finished' line
+        start = start_indices[i]  # Starting line for CC assignment
+        stop = start_indices[i + 1]  # End line for CC assignment (next assignment starts or 'Finished')
+        # Data
+        coil_info = {'indices': [], 'sequences': [], 'heptads': [], 'ambigous': False}
         for k in range(start, stop):
             line = lines[k].rstrip()
-            if entry in line and 'PRESENT' not in line and 'REPEATS' not in line:
+            if line.startswith('assigning heptad to helix'):
+                temp = line.split(' ')
+                # Get start and end residues for helix
+                try:
+                    if temp[6][0] == '-':
+                        start_res = -int(temp[6].split('-')[1])
+                        end_res = int(temp[6].split('-')[2].split(':')[0])
+                    else:
+                        start_res = int(temp[6].split('-')[0])
+                        end_res = int(temp[6].split('-')[1].split(':')[0])
+                    helix_id = int(temp[4])
+                    chain = line.split(':')[1]
+                except ValueError:
+                    coil_info['ambigous'] = True
+                seq_line = lines[k+2].rstrip("\n")
+                register_line = lines[k+3].rstrip("\n")
+                knobtype_line = lines[k+5].rstrip("\n")
+                seq = seq_line[9:]
+                if len(seq) != (end_res - start_res + 1):
+                    coil_info['ambigous'] = True
+                register = register_line[9:]
+                knobtype = knobtype_line[9:]
+                if method == 'heptads':
+                    left_marg = len(register) - len(register.lstrip())
+                    right_marg = len(register) - len(register.rstrip())
+                elif method == 'knobs':
+                    left_marg = len(knobtype) - len(knobtype.lstrip('-'))
+                    right_marg = len(knobtype) - len(knobtype.rstrip('-'))
+                elif method == 'overlap':
+                    left_marg1 = len(register) - len(register.lstrip())
+                    left_marg2 = len(knobtype) - len(knobtype.lstrip('-'))
+                    right_marg1 = len(register) - len(register.rstrip())
+                    right_marg2 = len(knobtype) - len(knobtype.rstrip('-'))
+                    left_marg = min(left_marg1, left_marg2)
+                    right_marg = min(right_marg1, right_marg2)
+                start_res = start_res + left_marg
+                end_res = end_res - right_marg
+                fseq = seq[left_marg:len(seq) - right_marg]
+                register = register[left_marg:len(seq) - right_marg].replace(' ', '-')
+                coil_info['heptads'].append(register)
+                coil_info['sequences'].append(fseq)
+                coil_info['indices'].append((helix_id, int(start_res), int(end_res), chain))
+            if 'length max' in line and 'PRESENT' not in line and 'REPEATS' not in line:
                 inf = re.findall('\((.*?)\)', line)
                 data = inf[1].split(' ')
                 oligomerization = data[1][0]
                 orientation = data[0]
                 coil_info['oligomerization'] = oligomerization
-                coil_info['orientation'] =  orientation
-            if line.startswith('extent of coiled coil packing:'):
-                temp = line.split(':')
-                chain = temp[2]
-                start_res = temp[1].replace(' ', '').split('-')[0]
-                end_res = temp[1].replace(' ', '').split('-')[1]
-                line_prev = lines[k-1].rstrip()
-                data_prev = line_prev.split(' ')
-                helix_id = data_prev[4]
-            if line.startswith('sequence'):
-                seq2 = line[9:]
-                hept = lines[k+1][9:]
-                partner = lines[k+2][9:]
-                if method == 'cc':
-                    ls = len(hept) - len(hept.lstrip())
-                    seq2 = seq2[ls:]
-                    hept = hept[ls:].rstrip()
-                    ts = len(seq2) - len(hept)
-                    if ts > 0:
-                        seq2 = seq2[:-ts]
-                    else:
-                        pass
-                    assert len(seq2) == len(hept)
-                    coil_info['heptads'].append(hept)
-                    coil_info['sequences'].append(seq2)
-                    coil_info['indices'].append((helix_id, int(start_res), int(end_res), chain))
-                elif method == 'kih':
-                    st = False
-                    partner_start = 0
-                    partner_end = 0
-                    for z in range(0, len(partner)):
-                        if partner[z] != '-' and partner[z] != '\n':
-                            #print(partner[z], z)
-                            if not st:
-                                st = True
-                                partner_start = z
-                            partner_end = z
-                    st = False
-                    hept_start = 0
-                    hept_end = 0
-                    for z in range(0, len(hept)):
-                        if hept[z] != ' ' and hept[z] != '\n':
-                            if not st:
-                                st = True
-                                hept_start = z
-                            hept_end = z
-                    fin_start = 0
-                    fin_end = 0
-                    if partner_start >= hept_start:
-                        fin_start = hept_start
-                    else:
-                        fin_start = partner_start
-                        offset = partner_start - hept_start
-                        print(partner_start, hept_start)
-                        start_res = int(start_res) + offset
-                    if partner_end >= hept_end:
-                        fin_end = partner_end
-                        offset = partner_end - hept_end
-                        end_res = int(end_res) + offset
-                    else:
-                        fin_end = hept_end
-                    coil_info['sequences'].append(seq2[fin_start:fin_end+1])
-                    fin_hept = ['-'for y in range(0, len(hept))]
-                    coil_info['heptads'].append(''.join(fin_hept))
-                    coil_info['indices'].append((helix_id, int(start_res), int(end_res), chain))
+                coil_info['orientation'] = orientation
             if line.startswith('	angle between helices'):
                 rel_data = line.split()
                 first_helix, second_helix, orientation = rel_data[3], rel_data[5], rel_data[8]
                 relations.append([first_helix, second_helix, orientation])
         all_coils.append(coil_info)
-    return all_coils, relations
+    if not all_coils:
+        return 0
+    else:
+        return all_coils, relations
