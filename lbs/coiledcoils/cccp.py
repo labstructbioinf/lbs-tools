7cd043b411315e258593f72c1a7d2b485da1e598
Johanna Haffner <j.haffner@cent.uw.edu.pl>
2019-08-13 15:12:18 +0200

    Created backup file, worked on documentation

--- lbs/coiledcoils/cccp.py
+++ lbs/coiledcoils/cccp.py
@@ -1,12 +1,58 @@
+# This file uses PEP8 conventions.
+# Variables that also appear in the CCCP Generator, but under a different name, are listed below.
+
+""" This script uses the CCCP generator developed by Grigoryan et al., and described by G. Grigoryan and W. F. DeGrado,
+"Probing Designability via a Generalized Model of Helical Bundle Geometry", J. Mol. Biol., 405(4): 1079-1100 (2011).
+See also http://grigoryanlab.org/cccp/.
+
+Description of variables:
+	Variables that also appear in the CCCP generator:
+
+	num_chains 	 - the number of chains.
+				   In the CCCP generator, it is alternately called cN and chains (k is used as a chain index)
+	chain_length - the length of each chain (in amino acid residues). Must be an integer.
+	r0  - the superhelical radius, called r0 in the CCCP Generator (R0 in the paper). Given in Angstrom.
+	r1  - the helical radius, called r1 in the CCCP Generator (R1 in the paper). Given in Angstrom.
+	w0  - the superhelical frequency, in degrees per residue.
+	w1  - the helical frequency, in degrees per residue.
+	a   - the pitch angle (called A in the CCCP generator, alpha_p in the paper), in degrees.
+	ph1 - the starting helical phase, in degrees. Can be passed as a scalar if its the same for all helices.
+		  Also referred to as the helical phase offset.
+	c_dir  - The orientation of all subsequent chains relative to the first chain.
+			 c_dir is a vector of length K = num_chains.
+			 c_dir has (alternating) entries 1 and 0, 1 indicating parallel and 0 indicating antiparallel orientation.
+			 The first entry must be 1 (the first chain is parallel to itself).
+			 Example: for any chain k > 1, if c_dir(k - 1) = 1, its orientation is parallel to the first chain.
+	z_off  - Alternately called zoff and Zoff in the CCCP generator, refers to the absolute vertical offset of a chain k
+			 relative to the first chain.
+			 z_off is a vector of length = num_chains - 1 = k - 1.
+			 Since absolute values might not be useful for fitting, the parameter z_off_type can be used to modify z_off.
+	z_type - Also referred to as varargin in the CCCP generator, used to modify z_off. Passed as a string.
+			 Description taken / paraphrased from the CCCP generator:
+			 zoffaa - z_off referes to the vertical offset between 'a' positions on opposing chains
+			 registerzoff - z_off will refer to points on the Crick curves that point directly into the interface
+			 apNNzoff - the Z offset is interpreted as between the N termini of chains 1 and k if k runs parallel to 1,
+			 and the N terminus of chain 1 and the C terminus of chain k if k is anti-parallel to 1.
+
+	Other variables defined for this script:
+	...
+
+	Where variables are renamed or reassigned in the functions and class defined below, clear indications are given 
+	in the function, class or method docstrings. Where no indication is given, or where the variable is simply
+	introduced, its meaning should match the one defined above.
+
+
+In this file, C-alpha backbones? are created from the CCCP generator and stored as ????
+"""
+
 import tempfile
-import sys
+import sys # use sys.exit(-1) to debug
 import math
 import numpy
 import subprocess
 import os
-import itertools
 import os.path
-import numpy as np
+import itertools
 
 from sympy import nsimplify
 from Bio.PDB.PDBParser import PDBParser
@@ -20,66 +66,46 @@ pihelix_p = 4.3985
 # Wychodzi 1.1689 (helanal), spore stdev - 0.3423
 pihelix_d = 1.1689
 	
-def in_the_core_new(ph1, w1, rep_len):
-	#ph1+=5
-	
-	#w1 = 102.8 # 7/2
-	#w1 = 98.18181818181819 # 11/3
-	#rep_len = 11
-	
-	#if ph1<0:
-	#	ph1 = 360 + ph1
-	
-	def adj(ang):
+
+def in_the_core(ph1, w1, rep_len):
+	""" This function calculates all residue angles in the given repeat length and outputs the smallest one.
+
+	It takes three inputs:
+	ph1 - starting helical phase (helical phase offset)
+	w1  - helical frequency in degrees per residue, examples: w1 = 102.8 for 7/2; w1 = 98.18181818181819 for 11/3;
+	rep_len - repeat length, passed as a float.
+	"""
+
+	def adj(ang: float) -> float:
+		""" This function checks whether an angle exceeds 180 degrees, and converts it to a negative angle if it does.
+		The input angle must be in degrees.
+
+		Examples: If the angle is 270 degrees, it will be converted to -90 degrees. 
+				  If the angle is 90 degrees, it will not be changed. 
+				  If the angle is 450 degrees, it will be converted to 90 degrees. 
+		"""
+
 		ang += 180
 	
 		if ang > 360:
-			c = int(ang/360)
+			c = int(ang/360) # Calculate by how many full turns the angle exceeds 360 degrees
 			return ang - (360*c) - 180 
 		else:
 			return ang - 180
 	
-	fullrepeat = [adj(ph1 + (w1*i)) for i in range(rep_len)]
-		
-	#print fullrepeat
-	
-	return sorted(fullrepeat, key = lambda x:abs(x))[0]
-	
-	#sys.exit(-1)
-	
-	
-def in_the_core(ph1, rep_len):
-	
-	#w1 = 360.0 / (1.0 * rep_len / rep_turn)
-	w1 = 360.0 / rep_len 
-	
+	full_repeat = [adj(ph1 + (w1*i)) for i in range(rep_len)] # Create vector with residue angles in degrees
 
-	l = [ph1]
+	return sorted(full_repeat, key = lambda x:abs(x))[0] # Return smallest residue angle in degrees
 
-	p = ph1
-	while True:
-		p = p + w1
-		if p >= 180: break
-		l.append(p)
-
-	p = ph1
-	while True:
-		p = p - w1
-		if p <= -180: break
-		l.append(p)
-	
-	assert len(l) == rep_len
-	temp = sorted(l, key = lambda x:abs(x))
-	#print temp
-	return temp[0]
 	
 def in_the_core_pandas(row):
-	return in_the_core_new(row['ph1'], row['w1'], row['rep_len'])
-	#return inTheCore(row['ph1'], row['rep_len'])
+	""" This function calls in_the_core (defined above), using values specified in a pandas data frame as inputs. """
+	return in_the_core(row['ph1'], row['w1'], row['rep_len'])
 
 def calcPER(P, tolerance = 0.01):
-	f = nsimplify(P, tolerance=tolerance)
-	return f.p, f.q
+	# TODO: What does this function do?
+	f0 = nsimplify(P, tolerance = tolerance)
+	return f0.p, f0.q
 
 def w0_P_to_p(w0, P):
 	return P / (1 + P*w0 / 360)
@@ -100,14 +126,14 @@ def calcphi(heptad, phi1):
 
 	return phi+9 # -26 -- 26 range
 
-def make_bb(f, outname, chainlen, chains  = ['A', 'B', 'C', 'D', 'E']):
+def make_bb(f, outname, chainlen, num_chains  = ['A', 'B', 'C', 'D', 'E']):
 	
-    fpdb = open(outname + '.pdb', 'w')
-    
-    atom_id = 1
-    res_id  = 1
-     
-    for coords in f:
+	fpdb = open(outname + '.pdb', 'w')
+
+	atom_id = 1
+	res_id  = 1
+
+	for coords in f:
 
 		coords = numpy.round(coords, 3)
 		fpdb.write('ATOM  ' + '{0: >5}'.format(str(atom_id)) + '{0: >4}'.format('CA') + ' {0: >4}'.format('GLY') + ' ' + chains[(res_id-1) / chainlen] + 
@@ -116,33 +142,33 @@ def make_bb(f, outname, chainlen, chains  = ['A', 'B', 'C', 'D', 'E']):
 		res_id  += 1
 		atom_id += 4
 
-    fpdb.close()
+	fpdb.close()
 
-def CCCPgenerate(outname, cN, chL, R0, R1, w0, w1, A, ph1, cr, dph0, Zoff, Zoff_type):
+def CCCPgenerate(outname, num_chains, chL, R0, R1, w0, w1, A, ph1, cr, dph0, z_off, z_type):
 	"""
 	dla zadanych parmetrow uruchamia CCCP + odbudowa backbone BBQ
 	"""
 
 	tf = tempfile.NamedTemporaryFile(delete=False)
-	tf.write("generateCrickBB(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, struct(\'%s\', 1))" % (cN, chL, R0, R1, w0, w1, A, ph1, cr, dph0, Zoff, Zoff_type))
+	tf.write("generateCrickBB(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, struct(\'%s\', 1))" % (num_chains, chL, R0, R1, w0, w1, A, ph1, cr, dph0, z_off, z_type))
 	tf.close()
 	
-	cmd = "octave-cli -q %s" % (tf.name)
+	cmd = "octave-cli -q %s" % tf.name
 	
 	curdir = os.getcwd()
 	os.chdir('/Users/sdh/apps/cccp')
 	
-	p = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+	p = subprocess.Popen(cmd, shell = True, stderr = subprocess.PIPE, stdout = subprocess.PIPE)
 	p.wait()
 	stdout, stderr = p.communicate()	
 	
 	if stderr != '':
-		print('cos poszlo nie tak...')
+		print('something went wrong...')
 		sys.exit(-1)
 	
 	os.chdir(curdir)
 	
-	coords = numpy.array([i.split() for i in stdout.split('\n')[2:-2]], dtype=numpy.dtype(float) )
+	coords = numpy.array([i.split() for i in stdout.split('\n')[2:-2]], dtype = numpy.dtype(float) )
 		
 	make_bb(coords, outname, chL)
 	
@@ -151,7 +177,7 @@ def CCCPgenerate(outname, cN, chL, R0, R1, w0, w1, A, ph1, cr, dph0, Zoff, Zoff_
 	p.wait()
 	
 	f = open('temp.seq', 'w')
-	print >>f, "A"*(cN*chL) 
+	print >>f, "A"*(num_chains*chL) 
 	f.close()
 	
 	cmd = '/Users/sdh/apps/scwrl4/Scwrl4 -s temp.seq -i %s.bbq.pdb -o %s.bbq.scwrl.pdb' % (outname, outname)
@@ -163,24 +189,24 @@ def CCCPgenerate(outname, cN, chL, R0, R1, w0, w1, A, ph1, cr, dph0, Zoff, Zoff_
 	return True
 	
 	# removing extreme positions
-	"""
+	#"""
 	
-	p = PDBParser(PERMISSIVE=1)
+	#p = PDBParser(PERMISSIVE=1)
 
-	structure_id = outname
-	filename = outname + '.bbq.pdb'
-	s = p.get_structure(structure_id, filename)
+	#structure_id = outname
+	#filename = outname + '.bbq.pdb'
+	#s = p.get_structure(structure_id, filename)
 	
-	for model in s:
-		for chain in model:
-			print chain
-			for id in [chL*(i)+1 for i in range(cN)]:
-				print id
+	#for model in s:
+		#for chain in model:
+			#print chain
+			#for id in [chL*(i)+1 for i in range(num_chains)]:
+				#print id
 				#chain.detach_child((' ', id, ' '))
-	"""
+	#"""
 				
 
-def CCCPgenerate_periodicity(P, name, cN, chL, R0, ph1, cr, dph0, Zoff, Zoff_type, residues_per_turn, d, R1, justprint=False):
+def cccp_generate_periodicity(P, name, num_chains, chL, R0, ph1, cr, dph0, z_off, z_type, residues_per_turn, d, R1, justprint=False):
 	"""
 	P - periodicity of a bundle
 	"""
@@ -191,25 +217,12 @@ def CCCPgenerate_periodicity(P, name, cN, chL, R0, ph1, cr, dph0, Zoff, Zoff_typ
 	
 	name = str(name)
 	
-	#cN = 2 # number of chains
-	#chL = 56 # number of residues
-	#R0 = 5.0 # superhelix radius
-	#R1 = 2.26 # helix radius
-	
-	#ph1 = -9
-	#cr = "[1]"
-	#dph0 = "[180]"
-	#Zoff = "[0]"
-	
-	
-	# wartosci dla a-helisy
+	# values ​​for a-helix
 	#residues_per_turn = 3.62705 # residues / turn in a straight helix
 	#d = 1.51 # axial rise per residue in a straight helix
 	
 	# in the science h-bond paper w1 is fixed (supp. data page 7)
 	w1 = 360.0 / P # as in CCCP paper (not "p"!); page 1087
-	
-	#w1 = 70
 	
 	# option 1 
 	#w0 = (- residues_per_turn / P + 1) / residues_per_turn * 360 # as in periodicity calculations in YadA paper
@@ -219,55 +232,55 @@ def CCCPgenerate_periodicity(P, name, cN, chL, R0, ph1, cr, dph0, Zoff, Zoff_typ
 	# gives identical results
 	
 	delta_t = 2*math.pi * ( 1/residues_per_turn - 1/P )
-	PITCH = (2*math.pi/delta_t) * math.sqrt(math.pow(d,2) - math.pow(R0*delta_t,2))
+	pitch = (2*math.pi/delta_t) * math.sqrt(math.pow(d,2) - math.pow(R0*delta_t,2))
 	
-	A = math.degrees(math.atan(2*math.pi*R0 / PITCH))
-	w0 = math.degrees((d * math.sin(math.radians(A))) / R0)
+	a = math.degrees(math.atan(2*math.pi*R0 / pitch))
+	w0 = math.degrees((d * math.sin(math.radians(a))) / R0)
 	
 	w0 = numpy.around(w0, decimals=3)
-	A = numpy.around(A, decimals=3)
+	a = numpy.around(a, decimals=3)
 	w1 = numpy.around(w1, decimals=3)
-	PITCH = numpy.around(PITCH, decimals=3)
+	pitch = numpy.around(pitch, decimals=3)
 
 	if not justprint:
-		CCCPgenerate(name, cN, chL, R0, R1, w0, w1, A, ph1, cr, dph0, Zoff, Zoff_type)
+		CCCPgenerate(name, num_chains, chL, R0, R1, w0, w1, a, ph1, cr, dph0, z_off, z_type)
 	
-	return ' '.join([str(i[0])+"_"+i[1] for i in zip([name, P, d, R0, w0, A, R1, w1, ph1, dph0, Zoff, cr, PITCH, cN], ['name', 'P', 'd', 'R0', 'w0', 'A', 'R1', 'w1', 'ph1', 'dph0', 'Zoff', 'cr', 'PITCH', 'chains'])])
+	return ' '.join([str(i[0])+"_"+i[1] for i in zip([name, P, d, R0, w0, a, R1, w1, ph1, dph0, z_off, cr, pitch, num_chains], ['name', 'P', 'd', 'R0', 'w0', 'a', 'R1', 'w1', 'ph1', 'dph0', 'z_off', 'cr', 'pitch', 'chains'])])
 
 
 class FitParser():
 
-	short_key = {'starting heptad position':'heptad',\
-					  'R1 (A)':'R1',\
-					  'R0 (A)':'R0',\
-					  'rise per residue (A)': 'd',\
-					  'alpha (rad)':'A',\
-					  'w0 (rad/res)':'w0',\
-					  'w1 (rad/res)':'w1',\
-					  'error':'fit_rmsd',\
-					  'pitch (A)':'PITCH',\
-					  'ph1 (rad)':'ph1',\
-					  'ph0 (rad)':'ph0',\
-					  'absolute ap zoff (A)':'ab_ap_zoff',\
-					  'dph0_p_ap (rad)':'dph0_p_ap',\
-					  'dph0_2 (rad)':'dph0_2',\
-					  'dph0_3 (rad)':'dph0_3',\
-					  'dph0_4 (rad)':'dph0_4',\
-					  'absolute zoff_4 (A)':'abs_zoff_4',\
-					  'absolute zoff_2 (A)':'abs_zoff_2',\
-					  'absolute zoff_3 (A)':'abs_zoff_3',\
-					  'message':'message',\
-					  'Z_aa for chain 2 (A)':'zaa2',\
-					  'Z_aa for chain 3 (A)':'zaa3',\
-					  'Z_aa for chain 4 (A)':'zaa4',\
-					  }
+
+	short_key = {'starting heptad position':'heptad',
+				 'R1 (A)':'R1',
+				 'R0 (A)':'R0',
+				 'rise per residue (A)': 'd',
+				 'alpha (rad)':'A',
+				 'w0 (rad/res)':'w0',
+				 'w1 (rad/res)':'w1',
+				 'error':'fit_rmsd',
+				 'pitch (A)':'PITCH',
+				 'ph1 (rad)':'ph1',
+				 'ph0 (rad)':'ph0',
+				 'absolute ap z_off (A)':'ab_ap_z_off',
+				 'dph0_p_ap (rad)':'dph0_p_ap',
+				 'dph0_2 (rad)':'dph0_2',
+				 'dph0_3 (rad)':'dph0_3',
+				 'dph0_4 (rad)':'dph0_4',
+				 'absolute z_off_4 (A)':'abs_z_off_4',
+				 'absolute z_off_2 (A)':'abs_z_off_2',
+				 'absolute z_off_3 (A)':'abs_z_off_3',
+				 'message':'message',
+				 'Z_aa for chain 2 (A)':'zaa2',
+				 'Z_aa for chain 3 (A)':'zaa3',
+				 'Z_aa for chain 4 (A)':'zaa4',
+				 }
 
 	def __init__(self, mode=1):
 		pass
 
-	def parse(self, filename, todegs=False, prefix=""):
+	def parse(self, filename, todegs = False, prefix = ""):
 
-	
 
 		# GENERAL (mode=0)
 
@@ -283,15 +296,15 @@ class FitParser():
 		#14 starting heptad position = b
 		#15 pitch (A) = 217.397019
 		#16 rise per residue (A) = 1.510448
-		#17 register Z-offset for chain 2 (A) = 1.785075
+		#17 register z_off for chain 2 (A) = 1.785075
 		#18 Z_aa for chain 2 (A) = 1.694279
-		#19 register Z-offset for chain 3 (A) = -0.000002
+		#19 register z_off for chain 3 (A) = -0.000002
 		#20 Z_aa for chain 3 (A) = -0.000002
-		#21 register Z-offset for chain 4 (A) = 1.785069
+		#21 register z_off for chain 4 (A) = 1.785069
 		#22 Z_aa for chain 4 (A) = 1.694273
-		#23 absolute zoff_2 (A) = 0.502193
-		#24 absolute zoff_3 (A) = 0.000005
-		#25 absolute zoff_4 (A) = 0.502185
+		#23 absolute z_off_2 (A) = 0.502193
+		#24 absolute z_off_3 (A) = 0.000005
+		#25 absolute z_off_4 (A) = 0.502185
 		#26 error = 0.000563
 
 
@@ -307,26 +320,26 @@ class FitParser():
 		#12 starting heptad position = b
 		#13 pitch (A) = 211.912218
 		#14 rise per residue (A) = 1.515943
-		#15 register Z-offset for chain 2 (A) = 2.369028
+		#15 register z_off for chain 2 (A) = 2.369028
 		#16 Z_aa for chain 2 (A) = 2.365918
-		#17 register Z-offset for chain 3 (A) = 2.369028
+		#17 register z_off for chain 3 (A) = 2.369028
 		#18 Z_aa for chain 3 (A) = 2.365918
-		#19 register Z-offset for chain 4 (A) = 0.000000
+		#19 register z_off for chain 4 (A) = 0.000000
 		#20 Z_aa for chain 4 (A) = 0.000000
-		#21 absolute ap zoff (A) = -3.507711
+		#21 absolute ap z_off (A) = -3.507711
 		#22 error = 0.700876
 
 
 		f = open(filename)
 		l = f.readline()
-		while l.find('structure summary:')<>-1:
+		while l.find('structure summary:') != -1:
 			l=f.readline()
 		
-		x = dict([x[:-1].split(' = ') for x in f.readlines()+[l]])
+		x = dict([x[:-1].split(' = ') for x in f.readlines() + [l]])
 		
 		for old_key in x.keys():
 			data = x.pop(old_key)
-			new_key = prefix+self.short_key[old_key]
+			new_key = prefix + self.short_key[old_key]
 			try:
 				data = float(data)
 				if todegs and new_key in ['w0', 'w1', 'A', 'ph1']:
@@ -353,10 +366,9 @@ class FitParser():
 			
 			except:
 				pass
-			print '%s' % val ,
-		print
+			print('%s' % val),
 
-	def renamewebCCCP(self, filename, phisteps, zsteps):
+	def renameweb_cccp(self, filename, phisteps, zsteps):
 		assert phisteps == zsteps
 		number = int(filename.split('.')[0])-1
 	
@@ -370,9 +382,9 @@ class FitParser():
 
 def tetramer(ap=False):
 
-	chains = 4
+	num_chains = 4
 	delta_phi0 = "[90,-180,-90]" 
-	pre_Zoff = "[{0},0,{0}]"
+	pre_z_off = "[{0},0,{0}]"
 
 	R0_range = numpy.linspace(7.36, 7.36, 1) # CCCP default 7.36 
 
@@ -381,13 +393,13 @@ def tetramer(ap=False):
 	else:
 		orient = "[0,1,0]" # antiparallel
 		
-	return chains, delta_phi0, pre_Zoff, orient, R0_range
+	return num_chains, delta_phi0, pre_z_off, orient, R0_range
 	
 if __name__ == "__main__":	
 
 
 	#Zoff_type = "registerzoff"
-	Zoff_type = "zoffaa"
+	z_type = "zoffaa"
 	
 	P = 3.5
 	pmax = 16
@@ -396,22 +408,22 @@ if __name__ == "__main__":
 	
 	f = open('bb_gen.log', 'w')
 	
-	for chains, delta_phi0, pre_Zoff, orient, R0_range in [tetramer(ap=True)]:
+	for num_chains, delta_phi0, pre_z_off, orient, R0_range in [tetramer(ap=True)]:
 		R0 = R0_range[0]
 		assert len(R0_range) == 1
 	
-		for Zoff_raw in numpy.linspace(1, 4, 7):
+		for z_off_raw in numpy.linspace(1, 4, 7):
 	
-			Zoff = pre_Zoff.format(Zoff_raw)
+			z_off = pre_z_off.format(z_off_raw)
 
 			pos = 0
-			for phi1 in numpy.linspace(-pmax, pmax, (pmax/2)+1):
+			for phi1 in numpy.linspace(-pmax, pmax, pmax/2 + 1):
 				phi1 = phi1 - 6.5		
 
-				name = "-".join([str(chains), str(Zoff_raw), str(pos)])
+				name = "-".join([str(num_chains), str(z_off_raw), str(pos)])
 
-				res = CCCPgenerate_periodicity(P, name, chains, length, R0, "[%s]" % ','.join(itertools.repeat(str(phi1), chains)),\
-													   orient, delta_phi0, Zoff, Zoff_type, 3.62705, ahelix_d, ahelix_R1, justprint=False)
+				res = cccp_generate_periodicity(P, name, num_chains, length, R0, "[%s]" % ','.join(itertools.repeat(str(phi1), num_chains)),\
+													   orient, delta_phi0, z_off, z_off_type, 3.62705, ahelix_d, ahelix_R1, justprint=False)
 				print >>f, res
 				print res
 				pos += 1
@@ -427,11 +439,10 @@ if __name__ == "__main__":
 	#print w0_P_to_p(-2.45, 3.5)
 	#sys.exit(-1)
 
-	#print inTheCore_new(44.331749962, 360/11.0, 11)
+	#print in_the_core(44.331749962, 360/11.0, 11)
 	
 	#pass
 
 
 	#print round(w0_to_P((np.degrees(float(sys.argv[1]))), 3.62125352),3)
 	#print calcPER(4.45, tolerance=0.025)
-	
