3188e0b6aaf58e8693bf3719f1414eeecb5a69d8
sdh <s.dunin-horkawicz@cent.uw.edu.pl>
2019-08-22 14:50:13 +0200

    adds fasta2nottruncated

---
+++ lbs/sequences/fasta2nottruncated.py
@@ -0,0 +1,42 @@
+
+from Bio import SeqIO
+import sys
+
+def removeTruncated(filename, outfilename, lrange, lfraction, rrange, rfraction, format='fasta'):
+
+	def gapfraction(s):
+		return round(1. * s.count('-') / len(s), 2)
+		
+	of = open(outfilename, 'w')
+	
+	removed, ok = 0, 0
+
+	for s in SeqIO.parse(filename, format):
+		l = s.seq[:lrange]
+		r = s.seq[-rrange:]
+		
+		lg = gapfraction(l)
+		rg = gapfraction(r)
+
+		
+		
+		
+		if lg <= lfraction and rg <= rfraction:
+			of.write(s.format(format))
+			ok+=1
+			status=''
+		else:
+			removed+=1
+			status='REMOVED'
+			
+		print ("{} {:>5} ....... {} {:>5} {}".format(l, lg, r, rg, status))
+
+	of.close()
+	
+	print ('{}/{} sequences removed'.format(removed, removed+ok))
+
+if __name__ == "__main__":
+	removeTruncated(sys.argv[1], sys.argv[2], 20, 0.7, 10, 0.7)
+	
+	
+	
