bd32dd064556e5408c4deb46046418d433c5fb1a
Staszek Dunin-Horkawicz <s.dunin-horkawicz@cent.uw.edu.pl>
2021-08-25 14:52:02 +0200

    ecod pdb_range fix

--- lbs/structures/ecod.py
+++ lbs/structures/ecod.py
@@ -5,34 +5,34 @@ def parse_pdb_ranges(pdb_ranges):
 			A:474-512,A:603-675
 			etc.
 	"""
-    
-    for pdb_range in pdb_ranges.split(','):
-        tmp = pdb_range.split(":")[1]
 
-        if tmp[0] == '-':
-            minus_start = True
-            tmp = tmp[1:]
-        else:
-            minus_start = False
+	for pdb_range in pdb_ranges.split(','):
+		tmp = pdb_range.split(":")[1]
 
-        if tmp.find('--')>0:
-            tmp = tmp.replace('--', '-')
-            minus_end = True
-        else:
-            minus_end = False
-            
-        if tmp.find('-') != -1:
-        	ecod_start, ecod_end = tmp.split("-")
+		if tmp[0] == '-':
+			minus_start = True
+			tmp = tmp[1:]
+		else:
+			minus_start = False
+
+		if tmp.find('--')>0:
+			tmp = tmp.replace('--', '-')
+			minus_end = True
+		else:
+			minus_end = False
+		
+		if tmp.find('-') != -1:
+			ecod_start, ecod_end = tmp.split("-")
 		else:
 			ecod_start = ecod_end = tmp
 
-        if minus_start:
-            ecod_start = '-' + ecod_start
-            
-        if minus_end:
-            ecod_end = '-' + ecod_end
-            
-        yield ecod_start, ecod_end
-        
-        
+		if minus_start:
+			ecod_start = '-' + ecod_start
+		
+		if minus_end:
+			ecod_end = '-' + ecod_end
+		
+		yield ecod_start, ecod_end
+	
+	
 
