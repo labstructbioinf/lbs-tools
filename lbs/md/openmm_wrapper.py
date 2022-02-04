6b64bf8b76b0476e56993979b8ae358a25e63479
Kamil <kamil.kaminski@cent.uw.edu.pl>
2022-02-04 18:00:42 +0100

    upd

--- lbs/md/openmm_wrapper.py
+++ lbs/md/openmm_wrapper.py
@@ -1,5 +1,5 @@
 import os
-from typing import NamedTuple, Tuple
+from typing import NamedTuple, Tuple, Union
 from tempfile import TemporaryFile
 
 import pandas as pd
@@ -11,6 +11,11 @@ from openmm.app import PME, HBonds, StateDataReporter, PDBReporter
 from openmm.app.pdbfile import PDBFile
 from openmm.unit import nanometer, picosecond, kelvin, Quantity
 from openmm.vec3 import Vec3
+try:
+    from pdbfixer import PDBFixer
+except ImportError as e:
+    pass
+    
 
 
 class Params:
@@ -26,7 +31,9 @@ class Params:
     frictionCoeff: float = 1 # 1/picosecond
     stepSize: float = 0.002 # picosecond
     numSteps: int = 10000
-        
+    saveStep: int = 1000 # how often openmm write snapshot
+    keepWater: bool = False
+    
         
 class OpenMM:
     '''
@@ -36,24 +43,32 @@ class OpenMM:
     def __init__(self, params: Params):
         self.params = params
 
-    def prepare_pdb(self, path_pdb: str):
+    def prepare_pdb(self, path_pdb: Union[str, PDBFile], rm_residue=[]):
         '''
         prepare pdb file: add hydrogens, add solvent
         return:
             pdb
         '''
-        assert os.path.isfile(path_pdb)
-        # load pdb file
-        pdb_tmp = PDBFile(path_pdb)
+        from_fixer = False
+        if isinstance(path_pdb, str):
+            assert os.path.isfile(path_pdb)
+            pdb_tmp = PDBFile(path_pdb)
+        else:
+            from_fixer = True
+            pdb_tmp = path_pdb
         # feed it into Modeller and add missing atoms
         # quasi centering
-        positions_arr = pdb_tmp.getPositions(True)
+        
+        positions_arr = self._asarray(pdb_tmp.positions._value)
         positions_arr = positions_arr - np.mean(positions_arr, axis=0)
         shift = np.array(self.params.boxSize)
-        shift = self._as_array(shift)
+        shift = self._asarray(shift)
         positions_arr += shift
         modeller = Modeller(pdb_tmp.topology, positions_arr)
-        _ = modeller.addHydrogens(self.forcefield)
+        if len(rm_residue) != 0:
+            modeller.delete(toDelete=rm_residue)
+        if not from_fixer:
+            _ = modeller.addHydrogens(self.forcefield)
         modeller.addSolvent(self.forcefield, boxSize=Vec3(*self.params.boxSize)*nanometer)
         return modeller
     
@@ -87,16 +102,20 @@ class OpenMM:
         simulation.context.setPositions(pdb.positions)
         simulation.minimizeEnergy()
         with open(_file, 'wt') as tmp_file:
-            simulation.reporters.append(PDBReporter(path_output, 1000))
-            simulation.reporters.append(StateDataReporter(tmp_file, 1000, step=True,
-                    potentialEnergy=True, density=True, temperature=True))
+            simulation.reporters.append(PDBReporter(path_output, self.params.saveStep))
+            simulation.reporters.append(StateDataReporter(tmp_file,
+                                                          self.params.saveStep,
+                                                          step=True,
+                                                          potentialEnergy=True,
+                                                          density=True,
+                                                          temperature=True))
             simulation.step(self.params.numSteps)
         df = pd.read_csv(_file)
         os.remove(_file)
         return df
     
     @staticmethod
-    def _as_array(iterable, unit = nanometer):
+    def _asarray(iterable, unit = nanometer):
         '''
         convert iterable to quantity array
         '''
@@ -105,6 +124,74 @@ class OpenMM:
         return arr
         
         
+class Patcher:
+    '''
+    Main class for fixing common pdb issues such as:
+    * remove HETATOMS
+    * add missing hydrogens
+    * remove/handle custom residues
+    '''
+    residues = (
+    'ALA', 'ARG', 'ASH', 'ASN',
+    'ASP', 'CYM', 'CYS', 'CYX',
+    'GLH', 'GLN', 'GLU', 'GLY',
+    'HID', 'HIE', 'HIP', 'HYP',
+    'HIS',
+    'ILE', 'LEU', 'LYN', 'LYS',
+    'MET', 'PHE', 'PRO', 'SER',
+    'THR', 'TRP', 'TYR', 'VAL'
+    )
+    def __init__(self, forcefield):
+        '''
+        params:
+            forcefield
+        '''
+        self.ff = forcefield
+        self.template_db = self.ff._templates
+    
+    def read_and_repair(self, path_pdb: str):
+        '''
+        params:
+            path_pdb (str) path to structrue
+        return:
+            pdb (PDBFixer object)
+            invalid_residues (list[residues]) residues to remove
+        '''
+        assert os.path.isfile(path_pdb)
+        fixer = PDBFixer(filename=path_pdb)
+        fixer.removeHeterogens(keepWater=False)
+        fixer.addMissingHydrogens()
+        invalid_residues = self._check_residues(fixer.topology)
+        
+        return fixer, invalid_residues
+        
+    def _check_residues(self, topology):
+        '''
+        iterates over structure for non-residue objects
+        '''
+        invalid_residues = list()
+        for residue in topology.residues():
+            if residue.name not in self.residues:
+                invalid_residues.append(residue)
+        return invalid_residues 
+    
+    def get_template_dict(self, name):
+        '''
+        return forcefield `name` templateData object
+        '''
+        if name in self.template_db:
+            template = self.template_db[name].atoms
+            map_name_type = {atom.name : atom.type for atom in template}
+        else:
+            map_name_type = None
+        return map_name_type
+    
+    def set_template_atom_type(self, template, map_name_type):
+        for atom in template.atoms:
+            atom.type = map_name_type[atom.name]
+        return template
+    
+    
     
 def find_residue_atoms(topology: Topology, chain: str, residue: str):
     '''
---
+++ lbs/md/params.py
@@ -0,0 +1,10 @@
+
+availabe_residues = (
+    'ALA', 'ARG', 'ASH', 'ASN',
+    'ASP', 'CYM', 'CYS', 'CYX',
+    'GLH', 'GLN', 'GLU', 'GLY',
+    'HID', 'HIE', 'HIP', 'HYP',
+    'ILE', 'LEU', 'LYN', 'LYS',
+    'MET', 'PHE', 'PRO', 'SER',
+    'THR', 'TRP', 'TYR', 'VAL'
+)
