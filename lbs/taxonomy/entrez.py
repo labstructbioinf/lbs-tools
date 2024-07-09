a95db9bf53b94b9ef7ea690ab35029fdea7f1b37
Staszek Dunin-Horkawicz <s.dunin-horkawicz@cent.uw.edu.pl>
2024-07-09 12:33:35 +0200

    upd

--- lbs/taxonomy/entrez.py
+++ lbs/taxonomy/entrez.py
@@ -31,20 +31,24 @@ class TaxonomyManager:
 				taxid_map[protein_id] = taxid
 		return taxid_map
 
-	def get_lineage(self, taxids):
-		lineage_map = {}
-		taxid_chunks = [list(taxids.values())[i:i + self.chunk_size] for i in range(0, len(taxids), self.chunk_size)]
+	def get_lineage(self, taxids: list, sleep=1):
+	
+		taxid_chunks = [taxids[i:i + self.chunk_size] for i in range(0, len(taxids), self.chunk_size)]
+
 		for chunk in tqdm(taxid_chunks):
+		
+			lineage_map = {}
+		
 			id_list = ','.join(chunk)
 			handle = Entrez.efetch(db="taxonomy", id=id_list, retmode="xml")
 			records = Entrez.read(handle)
-			time.sleep(1)
-			assert len(records) == len(chunk)
+			time.sleep(sleep)
+			
+			found = []
 
-			for record, taxid_ver in zip(records, chunk):
+			for record in records:
 				taxid = record['TaxId']
-				assert taxid_ver == taxid
-
+				found.append(taxid)
 				lineage = record['LineageEx']
 				full_lineage = {}
 				for item in lineage:
@@ -55,7 +59,14 @@ class TaxonomyManager:
 					full_lineage['OrganismName'] = organism_name
 				
 				lineage_map[taxid] = full_lineage
+				
+			if not set(found) <= set(chunk):
+				print('taxis has changed', set(found) - set(chunk))
 			
-		return lineage_map
+			not_found = set(chunk) - set(found)
+			if len(not_found)>0:
+				print('could not find', not_found)
+				
+			yield lineage_map 
 
 
